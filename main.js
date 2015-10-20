59d1978ce7a6e1d7cdb285604dce3872818e7760
adriana <adriana@negulescu.me>
2015-10-20 06:19:36 +0300

    improved version, probably the final one

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+
+		var expand = false;
+$(".menu-button").on("click", function(){
+				if(!expand){
+					$(".bla").animate({
+						marginLeft:"0px"
+						
+					},"fast", function(){
+						expand = true;
+					}).addClass("class");
+
+					$("#main").animate({
+						marginLeft:"320px"
+					},"fast");
+				} else {
+					$(".bla").animate({
+						marginLeft:"-320px"
+					},"fast",function(){
+						expand = false;
+					}).removeClass("class");
+
+					$("#main").animate({
+						marginLeft:"0"
+					},"fast");
+				}
+
+			});
+
+//start button toggle
 $(".open").addClass("red");
 $(".closed").on("click", function() {
 $(".closed").addClass("red");
@@ -7,12 +36,21 @@ $(".open").on("click", function() {
 $(".open").addClass("red");
 $(".closed").removeClass("red");
 });
-
-function myFunction() {
-    var date = new Date();
-    var elem = document.getElementById("demo");
-    var hour = addZero(d.getHours());
-    var minutes = addZero(d.getMinutes());
-    var seconds = addZero(d.getSeconds());
-    elem.innerHTML = hour + ":" + minutes + ":" + seconds;
+//end button toggle
+//start display hour
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
+var d = new Date();
+var elem_1 = document.getElementById("getHour");
+var elem_2 = document.getElementById("getHour2");
+var h = addZero(d.getHours());
+var m = addZero(d.getMinutes());
+var s = addZero(d.getSeconds());
+elem_1.innerHTML = h + ":" + m + ":" + s;
+elem_2.innerHTML = h + ":" + m + ":" + s;
+
+
